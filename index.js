4ed8f2e3e3a0d2c4a740ff138c751386a1b0e4f8
Swetha <Sweswetha1402@gmail.com>
2025-05-21 12:22:37 +0530

    deploy application

---
+++ index.js
@@ -0,0 +1,36 @@
+document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+
+    if(!username || !password){
+        showError('All Fields are required')
+    }
+    handleLogin(username, password)
+
+})
+
+function handleLogin(username, password){
+ if (username === 'test' && password == '123'){
+    showSuccess('Login Successful!')
+    setTimeout(() => {
+        
+    }, window.location.href = '/SimpleAuth/hello.html',1500);
+ }else{
+    showError('Invalid Credentials')
+ }
+}
+
+function showError(message){
+    const errorDiv = document.createElement('div');
+    errorDiv.className = 'alert error';
+    errorDiv.textContent = message;
+    document.querySelector('.container').prepend(errorDiv);
+}
+
+function showSuccess(message){
+    const successDiv = document.createElement('div');
+    successDiv.className = 'alert success';
+    successDiv.textContent = message;
+    document.querySelector('.container').prepend(successDiv);
+}
